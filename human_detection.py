a38c88d2c90955dede2081ac78896cfe5ce341a0
taki0412 <taki041210@naver.com>
2021-11-19 02:42:44 +0900

    [ADD] Return True or False

--- human_detection.py
+++ human_detection.py
@@ -5,11 +5,12 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
-
-# Download Model : https://pytorch.org/vision/stable/models.html
-model = models.detection.keypointrcnn_resnet50_fpn(pretrained=True).eval()
+import os, shutil
 
 def human_detection(path_1, path_2, path_3, path_4, path_5):
+    # Download Model : https://pytorch.org/vision/stable/models.html
+    model = models.detection.keypointrcnn_resnet50_fpn(pretrained=True).eval()
+
     IMG_SIZE = 480
     # 95% 이상이 되어야 사람으로 Detect
     THRESHOLD = 0.95
@@ -22,71 +23,53 @@ def human_detection(path_1, path_2, path_3, path_4, path_5):
     path_list.append(path_4)
     path_list.append(path_5)
 
-    # 사람의 위치 좌표
-    """
-    리스트 0~1 : 1번 이미지
-    리스트 2~3 : 2번 이미지
-    리스트 4~5 : 3번 이미지
-    리스트 6~7 : 4번 이미지
-    리스트 8~9 : 5번 이미지
-    """
-    x_coo = []
-    y_coo = []
-
     for i in range(0, 5):
         path = path_list[i]
-        # img_path = './data/demo/sleep_1.JPG'
         img = Image.open(path)
-        # img = img.transpose(Image.ROTATE_270)
         img = img.resize((IMG_SIZE, int(img.height * IMG_SIZE / img.width)))
 
         # 이미지를 텐서로 바꿔주기
         trf = T.Compose([T.ToTensor()])   # 이미지를 0~1의 값을 값는 텐서로 변경
         input_img = trf(img)
         # print(input_img.shape)  # [채널, 세로, 가로]
-
-        try:
-            # Inference
-            out = model([input_img])[0]
-            # print(out.keys())
-            """
-            dict_keys(['boxes', 'labels', 'scores', 'keypoints', 'keypoints_scores'])
-            우리는 boxes score를 사용하겠다.
-            """
-        except:
-            # print("사람이 감지되지 않았습니다.")
+        if RETURN(model, input_img, THRESHOLD) == False:
             return False
         else:
-            # Visualization
-            codes = [
-                Path.MOVETO,
-                Path.LINETO,
-                Path.LINETO
-            ]
-            fig, ax = plt.subplots(1, figsize=(16, 16))
+            pass
+    return True
 
-            for box, score in zip(out['boxes'], out['scores']):
-                score = score.detach().numpy()
+def RETURN(model, input_img, THRESHOLD):
+    try:
+        # Inference
+        out = model([input_img])[0]
+        # print(out.keys())
+        """
+        dict_keys(['boxes', 'labels', 'scores', 'keypoints', 'keypoints_scores'])
+        우리는 boxes score를 사용하겠다.
+        """
+    except:
+        return False
+    else:
+        # fig, ax = plt.subplots(1, figsize=(16, 16))
 
-                if score < THRESHOLD:
-                    continue
+        for box, score in zip(out['boxes'], out['scores']):
+            score = score.detach().numpy()
 
-                box = box.detach().numpy()
-                rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor='b', facecolor='none')
-                ax.add_patch(rect)
-                print('사람이 있음.')
-                # ax.imshow(img)
-                #plt.show()
+            # 사람일 확률
+            if score < THRESHOLD:
+                continue
 
-                # 0~1 : X좌표 | 2~3 : Y좌표
-                x_coo.append(box[0])
-                x_coo.append(box[1])
-                y_coo.append(box[2])
-                y_coo.append(box[3])
+            """ box 좌표
+            box 0 : x1
+            box 1 : y1
+            box 2 : x2
+            box 3 : y2 """
+            # box = box.detach().numpy()
 
-                print(x_coo)
-                print(y_coo)
-
-            return x_coo, y_coo
-
-human_detection('./data/demo/study_1.JPG', './data/demo/study_2.JPG', './data/demo/study_3.JPG', './data/demo/study_4.JPG', './data/demo/two_study.jpeg')
+            """ 사람 그리기
+            rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor='b', facecolor='none')
+            ax.add_patch(rect)
+            ax.imshow(img)
+            plt.show()
+            """
+            return True
