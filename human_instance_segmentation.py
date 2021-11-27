491a0b85bbe412a0c21a3ee56a599174b49cb53b
taki0412 <taki041210@naver.com>
2021-11-28 01:09:47 +0900

    [ADD] human_segmentation

--- human_instance_segmentation.py
+++ human_instance_segmentation.py
@@ -4,8 +4,6 @@ import numpy as np
 import torch
 from PIL import Image
 
-
-
 """ human segmentation은 posture classification의 input image가 될 것 입니다. """
 
 class human_segmentation(torch.utils.data.Dataset):
@@ -33,4 +31,41 @@ class human_segmentation(torch.utils.data.Dataset):
         # 색 mask를 binary 이진 마스크로 분할
         masks = mask == obj_ids[:, None, None]
 
+        # 각 mask의 box 좌표 가져오기
+        num_obj = len(obj_ids)
+        boxes = []
+        for i in range(num_obj):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.man(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, xmax, ymin, ymax])
 
+        # 모든것들을 tensor로 변환시키다.
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # 클래스 하나만 씀
+        labels = torch.ones((num_obj,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # 모든 instance가 겹치지 않는다고 가정
+        iscrowd = torch.zeros((num_obj,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
