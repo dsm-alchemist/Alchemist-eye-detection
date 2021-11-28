82d9db24dde4e342a9ad49b8d03159c7f6b20717
taki0412 <taki041210@naver.com>
2021-11-28 23:56:58 +0900

    [ADD] MASK R-CNN

--- load_model.py
+++ load_model.py
@@ -9,7 +9,8 @@ def load_model(model_num):
         model = models.segmentation.deeplabv3_resnet101(pretrained=True).eval()
         return model
     elif model_num == 2:
-        return "No Model"
+        model = models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        return model
     else:
         print("모델이 없습니다.")
         print("detection : 0 | segmentation : 1 | classification : 2")
