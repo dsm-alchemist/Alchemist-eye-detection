a1e09ceef61e8a01ca073a2381bf386323b1b730
taki0412 <taki041210@naver.com>
2021-12-08 13:40:17 +0900

    [FIX] COLOR

--- color_mask.py
+++ color_mask.py
@@ -7,10 +7,10 @@ def color_masks(image, classes='default'):
     b = np.zeros_like(image).astype(np.uint8)
 
     if classes == 'human':
-        r[image == 1], g[image == 1], b[image == 1] = colors[1]
+        r[image == 1], g[image == 1], b[image == 1] = colors[0]
         colored_mask = np.stack([r, g, b], axis=2)
         return colored_mask
     else:
-        r[image == 1], g[image == 1], b[image == 1] = colors[0]
+        r[image == 1], g[image == 1], b[image == 1] = colors[1]
         colored_mask = np.stack([r, g, b], axis=2)
         return colored_mask
