2d97a5571a0fccc79ada630ded265263d3c511e4
taki0412 <taki041210@naver.com>
2021-12-22 15:06:54 +0900

    [DELECT] Library

--- instance_segmentation.py
+++ instance_segmentation.py
@@ -1,5 +1,4 @@
 from instance_segmentation_model import instance_segmentation_model
-from get_path import get_path
 
 def instance_segmentation(path, threshold=0.95, url=False):
     img, _, _ = instance_segmentation_model(path, threshold, url)
