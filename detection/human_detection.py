3c9a230067b5eeeba156135c5fb2d265e2d0f0de
taki0412 <taki041210@naver.com>
2021-12-29 23:42:13 +0900

    [ADD] Visualization

---
+++ detection/human_detection.py
@@ -0,0 +1,35 @@
+# Import Library
+import torchvision.transforms as T
+from PIL import Image
+from detection.return_detect import return_detect
+from load_model import load_model
+from parameter import IMG_SIZE, THRESHOLD
+import requests
+from io import BytesIO
+from get_path import get_path
+
+def human_detection(path_1, path_2, path_3, path_4, path_5, url=False):
+    model = load_model(0)
+
+    # 이미지 path 리스트
+    path_list = get_path(path_1, path_2, path_3, path_4, path_5)
+
+    for i in range(0, 5):
+        if url:
+            response = requests.get(path_list[i])
+            img = Image.open(BytesIO(response.content))
+        else:
+            path = path_list[i]
+            img = Image.open(path)
+
+        img = img.resize((IMG_SIZE, int(img.height * IMG_SIZE / img.width)))
+
+        # 이미지를 텐서로 바꿔주기
+        trf = T.Compose([T.ToTensor()])   # 이미지를 0~1의 값을 값는 텐서로 변경
+        input_img = trf(img)
+        # print(input_img.shape)  # [채널, 세로, 가로]
+        if return_detect(model, img, input_img, THRESHOLD=0.65) == False:
+            return False
+        else:
+            pass
+    return True
---
+++ detection/return_detect.py
@@ -0,0 +1,41 @@
+# Import Library
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+def return_detect(model, img, input_img, THRESHOLD=0.65):
+    try:
+        # Inference
+        out = model([input_img])[0]
+        for box, score in zip(out['boxes'], out['scores']):
+            score = score.detach().numpy()
+
+            # 사람일 확률
+            if score > THRESHOLD:
+                return True
+            else:
+                return False
+    except:
+        return False
+    """ # Visualization 코드입니다. Try구문에 return True 대신 pass를 넣어주세요
+    else:
+        for box, score in zip(out['boxes'], out['scores']):
+            score = score.detach().numpy()
+
+            # 사람일 확률
+            if score > THRESHOLD:
+                fig, ax = plt.subplots(1, figsize=(16, 16))
+                # box 좌표
+                # box 0 : x1
+                # box 1 : y1
+                # box 2 : x2
+                # box 3 : y2
+                box = box.detach().numpy()
+
+                rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor='b',
+                                         facecolor='none')
+                ax.add_patch(rect)
+                ax.imshow(img)
+                plt.show()
+                return True
+            else:
+                return False """
