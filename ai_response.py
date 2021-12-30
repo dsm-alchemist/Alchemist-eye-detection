d2189a6781be9ecd333e4a6e667280ecc2b8b6ab
taki0412 <taki041210@naver.com>
2021-12-30 18:12:18 +0900

    [ADD] AI Response

--- ai_response.py
+++ ai_response.py
@@ -1,2 +1,8 @@
+# Import Library
 from detection.human_detection import human_detection
-from classification.human_classification import posture_classification
+from classification.human_classification import human_classification
+
+def ai_response(*args):
+    if not human_detection(*args):
+        return False
+    return human_classification(*args)
